bae3e20cf75791500d0ca0a36bb638563bedae92
sixsHalf <1740228138@qq.com>
2020-11-18 01:06:00 +0800

    add mock

--- src/lib/mock.js
+++ src/lib/mock.js
@@ -1,11 +1,17 @@
-// 使用 Mock
-var Mock = require('mockjs')
-var data = Mock.mock({
-    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
-    'list|1-10': [{
-        // 属性 id 是一个自增数，起始值为 1，每次增 1
-        'id|+1': 1
-    }]
-})
-// 输出结果
-console.log(JSON.stringify(data, null, 4))
+const delay = require('mocker-api/lib/delay'); // 延时 模拟请求异步问题
+const Mock = require('mockjs');
+
+const data = {
+    'GET /proxy': (req,res)=>{
+        res.status(200).json(Mock.mock({
+            '@id':1000
+        }))
+    },
+    'GET /proxy': (req,res)=>{
+        res.status(200).json(Mock.mock({
+            '@id':1001
+        }))
+    }
+    
+}
+module.exports = delay(data,2000)
